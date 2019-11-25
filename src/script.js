cfbedad87c61eb0c4033ea9c884ec7c755589eac
Jason Kao <jason.kao@columbia.edu>
2019-11-25 11:59:56 -0500

    d3 config

--- src/script.js
+++ src/script.js
@@ -1,26 +1,44 @@
 import enterView from 'enter-view';
 import textBalancer from 'text-balancer';
+import { select as d3Select } from 'd3-selection';
+import 'd3-jetpack/essentials';
 
 import { USE_COVER_HED } from '../config.yml';
 
-// Fade in navbar at scroll trigger
+/* Fade in navbar at scroll trigger */
 
 const navbar = document.getElementById('navbar');
+enterView({
+  selector: USE_COVER_HED ? '.headline' : '.step-deck',
+  offset: USE_COVER_HED ? 0.66 : 0.957,
+  enter: () => {
+    navbar.classList.remove('only-logo');
+  },
+  exit: () => {    
+    navbar.classList.remove('show-nav-links');
+    navbar.classList.add('only-logo');
+  },
+});
 
-// Mobile navbar hamburger trigger
+/* Mobile navbar hamburger trigger */
 
 export function hamburgerTrigger() {
   navbar.classList.toggle('show-nav-links');
 }
 
-// Text balance headline and deck
+/* Text balance headline and deck */
 
 textBalancer.balanceText('.headline, .deck, .image-overlay .image-caption');
 
-/* SVG icon stuff
+/* Scatter plot */
 
-const svg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10.29 12.3"><defs><style>.cls-1{fill:none;stroke:#999;stroke-miterlimit:10;}</style></defs><title>icon-close</title><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><line class="cls-1" x1="0.39" y1="0.32" x2="9.9" y2="11.99"/><line class="cls-1" x1="0.39" y1="11.99" x2="9.9" y2="0.32"/></g></g></svg>`;
-const svg64 = window.btoa(svg);
-console.log(`url('data:image/svg+xml;base64,${svg64}')`);
-
-*/
+const scatter = d3Select('#scatter');
+scatter
+  .append('div')
+  .st({
+    height: 100,
+    margin: '0 auto',
+    width: '90vw',
+    border: '2px dotted red',
+  });
+console.log(scatter);
